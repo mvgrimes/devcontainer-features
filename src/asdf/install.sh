2bc26456d4178e87c2ff4964667c16fd6e3fc100
Mark Grimes <mgrimes@tgfmail.org>
2025-10-13 10:59:16 -0400

    fix: uname -m is returning aarch64, we need arm64 so hard coding for now

--- src/asdf/install.sh
+++ src/asdf/install.sh
@@ -5,7 +5,7 @@ USERNAME=${USERNAME:-${_REMOTE_USER}}
 FEATURE_ID="asdf"
 LIFECYCLE_SCRIPTS_DIR="/usr/local/share/${FEATURE_ID}/scripts"
 VERSION=${VERSION:-"0.18.0"}
-ARCH="$( uname -m )"
+ARCH="arm64"
 
 if [ ! -e "/usr/local/bin/asdf" ]; then
   url="https://github.com/asdf-vm/asdf/releases/download/${VERSION}/asdf-v${VERSION}-linux-${ARCH}.tar.gz"
